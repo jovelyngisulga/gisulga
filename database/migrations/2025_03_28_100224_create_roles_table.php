ceee121ff80d2308672afc2202add793b97f50bb
gisulga jovelyn <gisulgajovelyn14@gmail.com>
2025-03-28 18:15:32 +0800

    added roles table

--- database/migrations/2025_03_28_100224_create_roles_table.php
+++ database/migrations/2025_03_28_100224_create_roles_table.php
@@ -11,9 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('role')->default('user');
-        });
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('role_name');
+            $table->string('url');
+         $table->timestamps();
+          });
     }
 
     /**
@@ -21,8 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-           $table->dropColumn('role');
-        });
+        Schema::dropIfExists('roles');
     }
 };
